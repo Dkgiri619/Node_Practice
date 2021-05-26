75d5306d4776fa2d7e4cadbf632146e98d52b7ca
Dkgiri619 <dkgiri619@gmail.com>
2021-05-26 23:00:15 +0530

    async and await

--- LModule1/automation/activity/login.js
+++ LModule1/automation/activity/login.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 const id = "gokot47302@itwbuy.com";
 const pass = "123456789";
-
+let idx,gcode;
 let tab;
 let browserPromise = puppeteer.launch({
     headless: false,
@@ -32,10 +32,94 @@ browserPromise.then(function(browser){
     return waitAndClick("a[id='base-card-1-link']");
 })
 .then(function(){
-    
+    return waitAndClick('a[data-attr1="warmup"]');
 })
+.then(function () {
+    return tab.waitForSelector(".js-track-click.challenge-list-item", {
+      visible: true,
+    });
+  })
+  .then(function () {
+    // tab.$() // document.querySelector;
+    return tab.$$(".js-track-click.challenge-list-item"); // it will run document.querySelectorAll in the browser and gives you array of all the elements
+  })
+  .then(function(allQuestionArr){
+      let allPendingPromises = [];
+      for(let i=0; i<allQuestionArr.length; i++){
+        let oneAtag = allQuestionArr[i];
+        let codeNamePromise = tab.evaluate(function(elem){return elem.getAttribute("href");}, oneAtag);
+        allPendingPromises.push(codeNamePromise);
+      }
+      return Promise.all(allPendingPromises);
+  })
+  .then(function(allQuesLinks){
+    let oneQuesSolvePromise = solveQuestion(allQuesLinks[0]);
+    return oneQuesSolvePromise;
+  })
 
 
+
+function getCode(){
+    return new Promise(function(scb, fcb){
+        let waitPromise = tab.waitForSelector(".hackdown-content h3", {visible:true});
+        waitPromise.then(function(){
+            return tab.$$(".hackdown-content h3");
+        })
+        .then(function(allCodeNamesElement){
+            let allCodeNamesPromise = [];
+            for(let i=0;i<allCodeNamesElement.length;i++){
+               let codeNamePromise = tab.evaluate(function(elem){ return elem.textContent;}, allCodeNamesElement[i]);
+               allCodeNamesPromise.push(codeNamePromise);
+            }
+            return Promise.all(allCodeNamesPromise);
+        })
+        .then(function(allCodeDiv){
+            for(let i=0;i<allCodeDiv.length;i++){
+                if(allCodeDiv[i]=="C++"){
+                    idx = i;
+                    break;
+                }
+            }
+            return tab.$$(".hackdown-content .highlight");
+        })
+        .then(function(allCodesDiv){
+            let cCode = allCodesDiv[idx];
+            return tab.evaluate(function(elem){
+                return elem.textContent;
+            },cCode);
+        })
+        .then(function(code){
+            gcode = code;
+            scb();
+        })
+        .catch(function(error){
+            fcb(error);
+        });
+    })
+}
+
+function pasteCode(){
+    return Promise(function(scb, fcb){
+        let     
+    })
+}
+function solveQuestion(link){
+    return new Promise(function(scb, fcb){
+        let gotoPromise = tab.goto("https://www.hackerrank.com"+link);
+        gotoPromise.then(function(){
+            return waitAndClick('div[data-attr2="Editorial"]');
+        })
+        .then(function(){
+            return getCode();
+        })
+        .then(function(){
+            return tab.click('div[data-attr2="Problem"]');
+        })
+        .then(function(){
+            return pasteCode();
+        })
+    })
+}
 function waitAndClick(selector){
     return new Promise(function(scb, fcb){
         let waitPromise = tab.waitForSelector(selector, {visible:true});
---
+++ LModule1/lec9_async-await/login.js
@@ -0,0 +1,45 @@
+const puppeteer = require("puppeteer");
+
+const id = "gokot47302@itwbuy.com";
+const pass = "123456789";
+let challenges = require("./challenges.js");
+(async function(){let browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"],
+  });
+let pages = await browser.pages();
+let tab = pages[0];
+await tab.goto("https://www.hackerrank.com/auth/login");
+await tab.type("#input-1", id);
+await tab.type("#input-2", pass);
+await tab.click( ".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+await tab.waitForSelector('div[data-analytics="NavBarProfileDropDown"]' , {visible:true});
+await tab.waitForTimeout(2000);
+let element = await tab.$('div[data-analytics="NavBarProfileDropDown"]');
+await element.click();
+await tab.waitForSelector('a[data-analytics="NavBarProfileDropDownAdministration"]' , {visible:true});
+await tab.click('a[data-analytics="NavBarProfileDropDownAdministration"]');
+await tab.waitForSelector('.nav-tabs.nav.admin-tabbed-nav li' , {visible:true});
+let bothLis = await tab.$$('.nav-tabs.nav.admin-tabbed-nav li');
+let manageChallengeLi = bothLis[1];
+await manageChallengeLi.click();
+await tab.waitForSelector('.btn.btn-green.backbone.pull-right' , {visible:true});
+let createChallengeElement = await tab.$('.btn.btn-green.backbone.pull-right');
+let createChallengeLink = await tab.evaluate( function(elem){ return elem.getAttribute("href"); }   ,  createChallengeElement)
+createChallengeLink = "https://www.hackerrank.com"+createChallengeLink;
+await tab.goto(createChallengeLink);
+let formGroup = await tab();
+// add one challenge
+// tab , challenge
+let challenge = challenges[0];
+// {
+//     "Challenge Name": "Pep_Java_1GettingStarted_1IsPrime",
+//     "Description": "Question 1",
+//     "Problem Statement": "Take as input a number n. Determine whether it is prime or not. If it is prime, print 'Prime' otherwise print 'Not Prime.",
+//     "Input Format": "Integer",
+//     "Constraints": "n <= 10 ^ 9",
+//     "Output Format": "String",
+//     "Tags": "Basics",
+//   }
+})();
