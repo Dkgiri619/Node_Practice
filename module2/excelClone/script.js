0dda6fbf6913f2b681929085a727cfbab1fdf897
Dkgiri619 <dkgiri619@gmail.com>
2021-07-16 14:21:29 +0530

    sheets updated

--- module2/excelClone/script.js
+++ module2/excelClone/script.js
@@ -1,24 +1,24 @@
-let cellsContainer = document.querySelector(".cells");
+let sheetsDB = [];
 let db;
+let cellsContainer = document.querySelector(".cells");
 let address = document.querySelector(".address");
 let lastSelect;
 let formulaInput = document.querySelector('.formula-bar');
 
 function intiCell() {
     let cellsContent = `<div class = "top-left-cell"></div>`;
-    
     cellsContent += `<div class = "top-row">`;
     for (let j = 0; j < 26; j++) {
         cellsContent += `<div class="top-row-cell">${String.fromCharCode(65 + j)}</div>`;
     }
     cellsContent += `</div>`;
-    
+
     cellsContent += `<div class = "left-col">`;
     for (let j = 0; j < 100; j++) {
         cellsContent += `<div class="left-col-cell">${j + 1}</div>`;
     }
     cellsContent += `</div>`;
-    
+
     cellsContent += `<div class="all-cell">`
     for (let i = 0; i < 100; i++) {
         cellsContent += `<div class="row">`;
@@ -41,15 +41,17 @@ function initDB() {
                 name: cellName,
                 value: "",
                 formula: "",
-                children: []
+                children: [],
+                parents: []
             }
             row.push(cellObject);
         }
         db.push(row);
     }
+    sheetsDB.push(db);
 }
-intiCell();
 initDB();
+intiCell();
 let topLeftCell = document.querySelector(".top-left-cell");
 let leftCol = document.querySelector(".left-col");
 let topRow = document.querySelector(".top-row");
@@ -68,32 +70,54 @@ formulaInput.addEventListener("blur", function (e) {
     let formula = e.target.textContent;
     if (formula) {
         let cellObj = getCellObjectFromElem(lastSelect);
+        if (cellObj.formula != formula) {
+            deleteFormula(cellObj);
+        }
         let calcValue = getValueFromFormula(formula, cellObj);
         lastSelect.textContent = calcValue;
         cellObj.value = calcValue;
         cellObj.formula = formula;
-    }
-});
-
-let allCell = document.querySelectorAll(".cell");
-for (let i = 0; i < allCell.length; i++) {
-    allCell[i].addEventListener("click", function (e) {
-        let cellObject = getCellObjectFromElem(e.target);
-        address.innerHTML = cellObject.name;
-        formulaInput.innerHTML = cellObject.formula;
-    });
-    allCell[i].addEventListener("blur", function (e) {
-        lastSelect = e.target;
-        let cellValue = e.target.textContent;
-        let cellObj = getCellObjectFromElem(e.target);
-        cellObj.value = cellValue;
-
 
         updateChildrenValue(cellObj.children);
-    });
+    }
+});
+let allCell = document.querySelectorAll(".cell");
+function attachClickAndBlurToCell(){
+    for (let i = 0; i < allCell.length; i++) {
+        allCell[i].addEventListener("click", function (e) {
+            let cellObject = getCellObjectFromElem(e.target);
+            address.innerHTML = cellObject.name;
+            formulaInput.innerHTML = cellObject.formula;
+        });
+        allCell[i].addEventListener("blur", function (e) {
+            lastSelect = e.target;
+            let cellValue = e.target.textContent;
+            let cellObj = getCellObjectFromElem(e.target);
+            if (cellObj.formula && cellValue != cellObj.value) {
+                deleteFormula(cellObj);
+                formulaInput.textContent = "";
+            }
+            cellObj.value = cellValue;
+            updateChildrenValue(cellObj.children);
+        });
+    }
 }
+attachClickAndBlurToCell()
 
+function deleteFormula(cellObj) {
+    cellObj.formula = "";
+    for (let i = 0; i < cellObj.parents.length; i++) {
+        let parent = cellObj.parents[i];
+        let parentCellObj = getCellObjectFromName(parent);
+        let updateChildren = parentCellObj.children.filter(function (childname) {
+            if (childname == cellObj.name) return false;
+            else return true;
+        });
+        parentCellObj.children = updateChildren;
+    }
+    cellObj.parents = [];
 
+}
 function updateChildrenValue(children) {
     for (let i = 0; i < children.length; i++) {
         let child = children[i];
@@ -121,6 +145,7 @@ function getValueFromFormula(formula, selfCellObj) {
             let value = cellObj.value;
             if (selfCellObj) {
                 cellObj.children.push(selfCellObj.name);
+                selfCellObj.parents.push(cellObj.name);
             }
             formula = formula.replace(fComps, value);
         }
@@ -131,4 +156,57 @@ function getCellObjectFromName(name) {
     let colid = name.charCodeAt(0) - 65;
     let rowid = Number(name.substring(1)) - 1;
     return db[rowid][colid];
+}
+
+
+// sheet functionalities
+
+let addSheet = document.querySelector(".add-button");
+let sheetLists = document.querySelector(".sheet-lists");
+let sid = 0;
+let defaultSheet = document.querySelector(".sheet");
+
+defaultSheet.addEventListener("click", function () { switchSheet(defaultSheet) });
+
+addSheet.addEventListener("click", addSheetFun);
+
+function addSheetFun() {
+    sid++;
+    document.querySelector(".active-sheet").classList.remove("active-sheet");
+    let sheet = document.createElement("div");
+    sheet.classList.add("sheet");
+    sheet.classList.add("active-sheet");
+    sheet.setAttribute("sid", sid);
+    sheet.innerHTML = `Sheet ${sid + 1}`;
+    sheet.addEventListener("click", function () { switchSheet(sheet) });
+    sheetLists.append(sheet);
+    initDB();
+    intiCell();
+    lastSelect = undefined;
+    attachAllEvents();
+}
+function attachAllEvents() {
+    cellsContainer = document.querySelector(".cells");
+    allCell = document.querySelectorAll(".cell");
+    topLeftCell = document.querySelector(".top-left-cell");
+    leftCol = document.querySelector(".left-col");
+    topRow = document.querySelector(".top-row");
+    attachClickAndBlurToCell();
+    
+}
+function switchSheet(currentSheet) {
+    if (currentSheet.classList.contains("active-sheet")) return;
+    document.querySelector(".active-sheet").classList.remove("active-sheet");
+    currentSheet.classList.add("active-sheet");
+    let sheetId = currentSheet.getAttribute("sid");
+    db = sheetsDB[sheetId];
+    console.log(db)
+    let lastCell = 0;
+    for(let i=0;i<db.length;i++){
+        let dbRow = db[i];
+        for(let j=0;j<dbRow.length;j++){
+            allCell[lastCell].textContent = dbRow[j].value;
+            lastCell++;
+        }
+    }
 }
